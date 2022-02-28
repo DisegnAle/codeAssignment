0163115b83e9a5267562de46b405580f18e68ac4
Alessio Scaiola <alessio.scaiola@rs2.com>
2022-02-28 07:05:20 +0100

    dev - add expression fn

--- js/calculator.js
+++ js/calculator.js
@@ -5,4 +5,17 @@ function onAddBtnClick() {
   addExpressionToDom(lastEvaluationsGroup, inputValue, evaluation);
 }
 
-
+function addExpressionToDom(lastEvaluationsGroup, inputValue, evaluation){
+  const newEvaluation = document.createElement('li');
+  const groupLength = lastEvaluationsGroup.childNodes.length;
+  const newIndex = groupLength > 0 ? groupLength-1 : 0;
+  const newID = 'evaluationID-'+newIndex;
+  newEvaluation.id = newID;
+  newEvaluation.innerHTML = `<b>${inputValue}</b> = <b>${evaluation}</b>`;
+  if(groupLength > 0){
+    const lastSecondEvaluation = lastEvaluationsGroup.firstChild;
+    lastEvaluationsGroup.insertBefore(newEvaluation,lastSecondEvaluation);
+  }else{
+    lastEvaluationsGroup.appendChild(newEvaluation);
+  }
+}
