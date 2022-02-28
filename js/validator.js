5bf57d4c04c8b6a00d334745350a3b06ae662578
Alessio Scaiola <alessio.scaiola@rs2.com>
2022-02-28 06:48:27 +0100

    dev - validation fn

--- js/validator.js
+++ js/validator.js
@@ -5,3 +5,31 @@ const notIncludedCharacter = /[^(0-9)(\-\+\*\/)(cos|sin|tan)(\(|\))(\s)]/g;
 const cannotStartWith = /^[\/\*]/;
 const endingWithSpace = /\s$/
 
+const validateMathemathicalExpression = (input) => {
+  let isValid = true;
+
+  if (endingWithSpace.test(input)) {
+    input = input.replace(/\s+$/, '');
+  }
+
+  if (containsParenthes.test(input)) {
+    if (closingParenthes.test(input) === false) {
+      isValid = false;
+    }
+  }
+
+  if (endingWithOperator.test(input) === true) {
+    isValid = false;
+  }
+
+  if (notIncludedCharacter.test(input) === true) {
+    isValid = false;
+  }
+
+  if (cannotStartWith.test(input) === true) {
+    isValid = false;
+  }
+
+  return isValid;
+
+};
